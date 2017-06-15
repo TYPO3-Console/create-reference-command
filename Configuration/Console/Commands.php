cbf55e552d0e614838f9ee08253df1e1f1de345b
Helmut Hummel <info@helhum.io>
2017-06-15 21:30:02 +0200

    Fix package name in command registration

--- Configuration/Console/Commands.php
+++ Configuration/Console/Commands.php
@@ -4,7 +4,7 @@ return [
         \Typo3Console\CreateReferenceCommand\Command\CommandReferenceCommandController::class,
     ],
     'runLevels' => [
-        'typo3-console/php-server-command:commandreference:*' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE,
+        'typo3-console/create-reference-command:commandreference:*' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE,
     ],
     'bootingSteps' => [
     ]
